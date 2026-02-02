7c6c9468ce4bc848fc2c0c096d47877eb7bbad08
Zabloun <eeyadsirsawy1234@gmail.com>
2026-02-02 20:03:07 +0200

    feat: add new frames and animation code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,21 @@
 extends CharacterBody2D
+
+@export var SPEED : int
 var direction
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down").normalized()
-	velocity = direction * 500
+	velocity = direction * SPEED
+	animationManager()
 	move_and_slide()
-	print(direction)
+	
+
+func animationManager():
+	if direction == Vector2.ZERO:
+		$AnimatedSprite2D.play("idle")
+	elif direction.x > 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	elif direction.x < 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
